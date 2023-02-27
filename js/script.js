74b811aa6066a93d66ad5ce94e68b39a59304c06
ewilkie <emilie.wilkie@gmail.com>
2023-02-27 18:19:25 +1100

    slider working

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
+let images = document.querySelectorAll(".carousel-item");
+let p = images.length;
+let i = 0;
 
-var images = document.querySelectorAll(".carousel-item");
 
 function slideLeft() {
-
-let currentImage = document.querySelector(".active"); 
-let nextIndex = Array.from(images).findIndex(item => item.classList.contains('active')) + 1;
-// if not last item 
-if (nextIndex < images.length) {
-    let nextImage = images[nextIndex];
-    imageTransition(currentImage,nextImage)
-    }else {
-        nextImage = images[0];    
-        imageTransition(currentImage,nextImage);
-    }
-};
-
-function imageTransition(currentImage,nextImage){
-    //currentImage.style.transform = "translateX(-100%)";
-    //nextImage.style.transform = "translateX(-100%)";
-    nextImage.classList.add("active")
-    currentImage.classList.remove("active")
-
-    console.log(nextImage)
-    console.log(currentImage)
+    // first item
+    if (i === 0) {
+        images[i].style = "left:0;z-index:3"; // move current image 
+        images[i + 1].style = "left:100%;z-index:1";
+        images[p - 1].style = "left:-100%;z-index:1"; // last      
+        i++
+    // not first not last    
+    } else if (i > 0 && i < p - 1) {
+        images[i - 1].style = "left:-100%;z-index:1";
+        images[i].style = "left:0;z-index:3";
+        images[i + 1].style = "left:100%;z-index:1";
+        i++;
+    // second last item     
+    } else if (i === p - 1) {
+        images[i - 1].style = "left:-100%;z-index:1";
+        images[i].style = "left:0;z-index:3";
+        i = 0;
+        images[i].style = "left:100%;z-index:1";
+  }
 }
 
-
-
 // Call the slideLeft function every 5 seconds
-setInterval(slideLeft, 1000);
+setInterval(slideLeft, 3000);
+
